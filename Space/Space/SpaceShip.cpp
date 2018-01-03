e82bcfc28713db444a8581e363c48bb7d5b09754
Charles Gingras <32151501+charlesgingras@users.noreply.github.com>
2018-01-03 15:07:46 -0500

    Add files via upload

--- Space/Space/SpaceShip.cpp
+++ Space/Space/SpaceShip.cpp
@@ -1,4 +1,5 @@
 #include "SpaceShip.h"
+
 	/*param pPosition: starting vertical position of spaceShip object*/
 	SpaceShip::SpaceShip(float pPosition)
 	{
@@ -9,6 +10,8 @@
 		aAmmo = std::vector<Ammunition*>();
 		aAmmo.push_back(new RegBullet);
 		ammoPtr = (aAmmo.front());
+		aHealth = 3;
+		aSpeed = 0.3f;
 	}
 	/*Increments remaining health by one unit*/
 	void SpaceShip::repairShip()
@@ -16,14 +19,14 @@
 		aHealth++;
 	}
 	/*Moves the spaceShip object vertically*/
-	void SpaceShip::moveUp(float p, int pHeight)
+	void SpaceObjects::moveUp(float p, int pHeight)
 	{
 		if (aShip.getPosition().y < 5 && p < 0) {}
 		else if (aShip.getPosition().y > pHeight - 5 && p > 0) {}
 		else aShip.move(0.0, p*aSpeed);
 	}
 	/*Moves the spaceShip object horizontally*/
-	void SpaceShip::moveRight(float p, int pWidth)
+	void SpaceObjects::moveRight(float p, int pWidth)
 	{
 		if (aShip.getPosition().x < 5 && p < 0) {}
 		else if (aShip.getPosition().x > pWidth - 5 && p > 0) {}
@@ -35,7 +38,7 @@
 		ammoPtr = aAmmo[pIndex];
 	}
 	/*Take damage: called when object is hit by a Projectile*/
-	void SpaceShip::getHit(int pDamage)
+	void SpaceObjects::getHit(int pDamage)
 	{
 		aHealth -= pDamage;
 		if (aHealth < 0)
@@ -44,12 +47,12 @@
 		}
 	}
 	/*Returns a Vector2f giving the position of the spaceShip object*/
-	sf::Vector2f SpaceShip::getPosition()
+	sf::Vector2f SpaceObjects::getPosition()
 	{
 		return aShip.getPosition();
 	}
 	/*Returns the sprite of the spaceShip object*/
-	sf::Sprite SpaceShip::getShip()
+	sf::Sprite SpaceObjects::getShip()
 	{
 		return aShip;
 	}
@@ -66,6 +69,17 @@
 	Ammunition* SpaceShip::getAmmoPtr()
 	{
 		return ammoPtr;
+	}
+	/*@param: pX, pY represent the X and Y positions of the asteroid
+			  size is a ratio of 0 to 1 to determine the size of the sprite*/
+	Asteroid::Asteroid(float pX, float pY, float size)
+	{
+		aSpeed = 0.1f; 
+		aHealth = 1;
+		aShip.setTextureRect(sf::IntRect(1149, 0, 1565, 390));
+		aShip.setTexture(*(allTextures->getTexture()), false);
+		aShip.setScale(size, size);
+		aShip.setPosition(pX, pY);
 	}
 
 
--- Space/Space/SpaceShip.h
+++ Space/Space/SpaceShip.h
@@ -5,30 +5,45 @@
 
 #include<TGUI/TGUI.hpp>
 
-
+class SpaceObjects
+{
+protected:
+	TextureRefs* allTextures = &(TextureRefs::getTextRefs());
+	sf::Sprite aShip;
+	double aSpeed;
+	const int MAX_HEALTH = 5;
+	int aHealth;
+public:
+	void moveUp(float p, int pHeight);
+	void moveRight(float p, int pWidth);
+	void getHit(int pDamage);
+	sf::Vector2f getPosition();
+	sf::Sprite getShip();
+	SpaceObjects() {};
+	~SpaceObjects() {};
+	
+};
 /*The spaceShip class is used to represent space ships (surprise...);
 it is used both by AI and players*/
-class SpaceShip
+class SpaceShip: public SpaceObjects
 {
 private:
-	TextureRefs* allTextures = &(TextureRefs::getTextRefs());
 	std::vector<Ammunition*> aAmmo;
-	sf::Sprite aShip;
 	Ammunition* ammoPtr;
-	double aSpeed = 0.3f;
-	const int MAX_HEALTH = 5;
-	int aHealth = 3;
 
 public:
 	void repairShip();
-	void moveUp(float p, int pHeight);
-	void moveRight(float p, int pWidth);
 	void changeLoadOut(int pIndex);
-	void getHit(int pDamage);
 	Projectile* shoot();
-	sf::Vector2f getPosition();
-	sf::Sprite getShip();
 	SpaceShip(float pPosition);
 	~SpaceShip() { delete ammoPtr; };
 	Ammunition* getAmmoPtr();
+};
+class Asteroid : public SpaceObjects
+{
+private:
+	int aDamage = 1;
+public:
+	Asteroid(float pX, float pY, float size);
+	~Asteroid();
 };
